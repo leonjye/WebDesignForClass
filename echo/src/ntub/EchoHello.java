d6a2e796bf6c61848df97b939e9fca1badc3e34f
User <User@DESKTOP-QQBLP6K>
2018-10-12 10:42:05 +0800

    hello world

---
+++ echo/src/ntub/EchoHello.java
@@ -0,0 +1,53 @@
+package ntub;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EchoHello
+ */
+@WebServlet("/EchoHello")
+public class EchoHello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EchoHello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		Enumeration<String> enums =  request.getParameterNames();
+		response.getWriter().append("\n");
+		while(enums.hasMoreElements()) {
+			String name = enums.nextElement();
+			String[] values = request.getParameterValues(name);
+			response.getWriter().append(name + ": ");
+			response.getWriter().append(String.join(", ", values));
+			response.getWriter().append("\n");
+		}
+		
+	}
+
+}
